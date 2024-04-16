d37331f182883418c47744a87d0da26c166ab68d
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2024-04-16 10:44:21 +0200

    Slap Detection
    On collision trigger on head collider which plays sound and makes the character ragdoll

---
+++ Assets/Scripts/ToggleRagdoll.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRagdoll : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public ConfigurableJoint[] joints;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            audioSource.Play();
+
+            foreach (var joint in joints)
+            {
+                JointDrive jointXDrive = joint.angularXDrive;
+                jointXDrive.positionSpring = 0f;
+                joint.angularXDrive = jointXDrive;
+
+                JointDrive jointYZDrive = joint.angularYZDrive;
+                jointYZDrive.positionSpring = 0f;
+                joint.angularYZDrive = jointYZDrive;
+            }
+        }
+    }
+}
