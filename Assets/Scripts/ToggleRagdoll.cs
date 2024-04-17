51a187d3bdc5df87fa6c1a8ab2073bd773517574
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2024-04-17 15:47:40 +0200

    Respawn
    Coroutine after slapping to respawn the slapped. Still needs tweaking

--- Assets/Scripts/ToggleRagdoll.cs
+++ Assets/Scripts/ToggleRagdoll.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class ToggleRagdoll : MonoBehaviour
 {
     AudioSource audioSource;
-    Rigidbody rb;
+    Rigidbody headRB;
+    
     float forceAmount = 1000f;
+    float scoreMultiplier = 1.0f;
 
     public Healthbar healthbar;
     public float maxHealth;
     float currentHealth;
 
     public ConfigurableJoint[] joints;
+    public Rigidbody[] rigidbodies;
+
+    private Vector3 ResetPosition;
+    private Quaternion ResetRotation;
+    public GameObject slappedParent;
+
+    [SerializeField]
+    private GameObject SpawnPoint;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        rb = GetComponent<Rigidbody>();
+        headRB = GetComponent<Rigidbody>();
 
         currentHealth = maxHealth;
+
+        ResetPosition = SpawnPoint.transform.position;
+        ResetRotation = SpawnPoint.transform.rotation;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -41,11 +54,14 @@ public class ToggleRagdoll : MonoBehaviour
 
             Debug.Log(collision.GetContact(0).normal);
 
-            float score = collision.relativeVelocity.magnitude * forceAmount;
+            float score = collision.relativeVelocity.magnitude * scoreMultiplier;
             Debug.Log($"Score: {score}");
             TakeDamage(score);
 
-            rb.AddForce(collision.GetContact(0).normal * collision.relativeVelocity.magnitude * forceAmount);
+            headRB.AddForce(collision.GetContact(0).normal * collision.relativeVelocity.magnitude * forceAmount);
+            Debug.Log($"Score: {collision.relativeVelocity.magnitude * forceAmount}");
+
+            StartCoroutine(Respawn(3));
         }
     }
 
@@ -53,5 +69,28 @@ public class ToggleRagdoll : MonoBehaviour
     {
         currentHealth -= damage;
         healthbar.UpdateHealthbar(currentHealth / maxHealth);
+    }
+
+    private IEnumerator Respawn(float waitTime)
+    {
+        Debug.Log("Start");
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("End");
+
+        foreach (var joint in joints)
+        {
+            JointDrive jointXDrive = joint.angularXDrive;
+            jointXDrive.positionSpring = 500f;
+            joint.angularXDrive = jointXDrive;
+
+            JointDrive jointYZDrive = joint.angularYZDrive;
+            jointYZDrive.positionSpring = 500f;
+            joint.angularYZDrive = jointYZDrive;
+        }
+
+        slappedParent.transform.position = ResetPosition;
+        slappedParent.transform.rotation = ResetRotation;
+        // rigidBody.velocity = Vector3.zero;
+        // rigidBody.angularVelocity = Vector3.zero;
     }
 }
