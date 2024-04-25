b4cfaa06026b12cee819bf64bccc41291bc565df
MatDevelopment <95708125+MatDevelopment@users.noreply.github.com>
2024-04-25 15:39:22 +0200

    SpeakerVoiceSystem + Non-3D sounds setup
    Made a system optimization for easy integration with audio clips.
    Also made some scene optimizations.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -10,17 +10,26 @@ public class AudioManager : MonoBehaviour
     {
         foreach (Sound s in sounds)
         {
-            s.source = gameObject.AddComponent<AudioSource>();     //Tager fat i Audio Source komponenten på et gameObject
-            s.source.clip = s.clip;         //Sætter audio klippet fra audio source component til s.clip
+            //s.source = gameObject.AddComponent<AudioSource>();     //Tager fat i Audio Source komponenten pï¿½ et gameObject
+            s.source = gameObject.GetComponent<AudioSource>();
+            //s.source.clip = s.clip;         //Sï¿½tter audio klippet fra audio source component til s.clip
 
-            s.source.volume = s.volume;     //Sætter volume fra audio source component til s.volume
-            s.source.pitch = s.pitch;       //Sætter pitch fra audio source til s.pitch
+            //s.source.volume = s.volume;     //Sï¿½tter volume fra audio source component til s.volume
+            //s.source.pitch = s.pitch;       //Sï¿½tter pitch fra audio source til s.pitch
         }
     }
 
-    public void Play (string name)       //Tilføjer en måde at spille en specifik sound ud fra den string name
+    public float Play (string name)       //Tilfï¿½jer en mï¿½de at spille en specifik sound ud fra den string name
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);     //Finder den sound i 'sounds' array som har det samme navn som string name (i denne void) og opbevarer den fundne sound i variablen 's'
+        s.source.clip = s.clip;
+        s.source.volume = s.volume;
+        s.source.pitch = s.pitch;
+        
         s.source.Play();
+            
+        float clipLength = s.source.clip.length;
+        
+        return clipLength;
     }
 }
